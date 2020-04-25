81077c9c26fb11fb3aedff31a1cf4a5f15c6a03c
Darren Arcangel <darrenarcangel@gmail.com>
2020-04-25 10:40:17 -0700

    finished intro to JS

---
+++ section_10/ageCalculatorScript.js
@@ -0,0 +1,4 @@
+var yearsAge = prompt("How old are you?");
+var daysAge = yearsAge * 365;
+
+alert ("You are " + yearsAge + " years old and " + daysAge + "days old");
---
+++ section_10/stalkerScript.js
@@ -0,0 +1,13 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What is your last name?");
+var fullName = firstName + " " + lastName;
+
+var age = prompt("How old are you?");
+
+//alert("Nice to meet you " + firstName + " " + lastName);
+alert("Nice to meet you " + fullName);
+alert("You are " + age + " years old");
+
+//console.log("Nice to meet you " + firstName + " " + lastName);
+console.log("Nice to meet you " + fullName);
+console.log("You are " + age + " years old");
