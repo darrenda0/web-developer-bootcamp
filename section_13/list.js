7c003492dceeac90fb6e91d1f104e20f7a01dcbc
Darren Arcangel <darrenarcangel@gmail.com>
2020-06-14 12:54:28 -0700

    Adding Array section and files

---
+++ section_13/list.js
@@ -0,0 +1,20 @@
+//console.log("Connected");
+
+var todos = ["Buy New Turtle"];
+
+var input = prompt("What would you like to do?");
+
+while(input !== "quit") {
+//handle input
+if(input === "list") {
+    console.log(todos);
+} else if(input === "new") {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+}
+//ask again for new input
+var input = prompt("What would you like to do?");
+}
+console.log("OK, you quit the app");
